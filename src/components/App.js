807cf30ac756b27ee6e0ae21508d7620b314d9d7
Supul Kalhara <supulkalhara7@gmail.com>
2022-12-19 15:05:02 +0530

    contact saved local and retrieved DONE

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,11 @@ function App() {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
-  const addContactHandler = (contact) => {
-    console.log(contact);
-    setContacts([...contacts, contact]);
-  };
-
-  useEffect(() => {
+  const addContactHandler = (contact) => {    
+    setContacts(oldContacts =>[...oldContacts,contact]);
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-  },[contacts]);
-
+  };
+  
   useEffect(() => {
     const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if(retriveContacts) setContacts(retriveContacts);
